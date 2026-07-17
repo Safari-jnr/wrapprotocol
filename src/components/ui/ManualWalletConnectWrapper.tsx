b1c99ce04bb132dd2535a2b11631955b2b29631e
Safari-jnr <faruqadegboyega481@gmail.com>
2026-07-16 23:41:04 -0700

    fix: use relative import in ManualWalletConnectWrapper

--- src/components/ui/ManualWalletConnectWrapper.tsx
+++ src/components/ui/ManualWalletConnectWrapper.tsx
@@ -4,7 +4,7 @@ import dynamic from "next/dynamic";
 
 export const ManualWalletConnectLazy = dynamic(
   () =>
-    import("@/components/ui/ManualWalletConnect").then((m) => ({
+    import("./ManualWalletConnect").then((m) => ({
       default: m.ManualWalletConnect,
     })),
   { ssr: false }
